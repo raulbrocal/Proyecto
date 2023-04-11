71550fc89865778fa93098627b45199b1be31b96
raulbrocal <raulbrocalberral16@gmail.com>
2023-04-11 19:54:50 +0200

    Actualización de la base de datos

--- sql/DDL_restaurant.sql
+++ sql/DDL_restaurant.sql
@@ -3,59 +3,81 @@ CREATE DATABASE IF NOT EXISTS restaurantDB;
 USE restaurantDB;
 
 CREATE TABLE restaurant (
-  name VARCHAR(255) NOT NULL,
-  address VARCHAR(255) NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  country VARCHAR(255) NOT NULL,
-  phone INT(8) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  social VARCHAR(255) NOT NULL,
-  closing_day INT(1) NOT NULL,
-  opening_time TIME NOT NULL,
-  closing_time TIME NOT NULL
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    phone INT(8) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    social VARCHAR(255) NOT NULL,
+    closing_day INT(1) NOT NULL,
+    opening_time TIME NOT NULL,
+    closing_time TIME NOT NULL
 );
 
-CREATE TABLE T_Client (
-    jugadorId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombreCompleto VARCHAR(200) DEFAULT NULL,
-    usuario VARCHAR(100),
-    CONSTRAINT T_Jugador_ibfk_1 FOREIGN KEY (usuario)
-        REFERENCES T_Usuario (usuario)
+CREATE TABLE reservation (
+    reservation_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    number_of_people INT(11) UNSIGNED NOT NULL,
+    indoor BOOLEAN NOT NULL DEFAULT TRUE,
+    comment VARCHAR(255)
 );
 
-CREATE TABLE T_Torneo (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombreTorneo VARCHAR(100) DEFAULT NULL,
-    fecha DATE NULL,
-    estado ENUM('No finalizado', 'Finalizado') DEFAULT 'No finalizado',
-    numJugadores INT DEFAULT 0,
-    campeon VARCHAR(100) DEFAULT 'Por decidir ...'
+CREATE TABLE user (
+    userId INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    profile ENUM('CLIENT', 'EMPLOYEE') NOT NULL DEFAULT 'CLIENT'
 );
 
- CREATE TABLE T_Partido (
-    partidoId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ronda ENUM('Cuartos', 'Semifinal', 'Final') NOT NULL,
-    torneoId INT,
-    jugadorA INT,
-    jugadorB INT,
-    ganador INT DEFAULT NULL,
-    CONSTRAINT T_Partido_ibfk_1 FOREIGN KEY (jugadorA)
-        REFERENCES T_Jugador (jugadorId),
-    CONSTRAINT T_Partido_ibfk_2 FOREIGN KEY (jugadorB)
-        REFERENCES T_Jugador (jugadorId),
-    CONSTRAINT T_Partido_ibfk_3 FOREIGN KEY (torneoId)
-        REFERENCES T_Torneo (ID)
-        ON DELETE CASCADE
+CREATE TABLE dinnerTable (
+    number INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    description VARCHAR(255),
+    capacity INT NOT NULL,
+    FOREIGN KEY (userId)
+        REFERENCES user (userId)
 );
 
--- Ejecutar test.php
--- Insertar jugadores
+CREATE TABLE menu (
+    menu_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255)
+);
 
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Javi Abandono', 'javi');
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Sergio Serrano', 'sergio');
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Daniel Okolo', 'okolo');
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Yeray Rus', 'yeray');
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Stewart Jordi', 'stewart');
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Adrián Castillo', 'castillo');
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Fernando Alonso', 'fernando');
-INSERT INTO T_Jugador (nombreCompleto, usuario) VALUES ('Jaume Altazona', 'jaume');
+CREATE TABLE meal (
+    meal_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    price DECIMAL(10 , 2 ),
+    menu_id INT,
+    FOREIGN KEY (menu_id)
+        REFERENCES menu (menu_id)
+);
+
+CREATE TABLE drink (
+    drink_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    alcoholic BOOLEAN NOT NULL DEFAULT FALSE,
+    description VARCHAR(255),
+    price DECIMAL(10 , 2 ),
+    menu_id INT,
+    FOREIGN KEY (menu_id)
+        REFERENCES menu (menu_id)
+);
+
+CREATE TABLE dessert (
+    dessert_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    price DECIMAL(10 , 2 ),
+    menu_id INT,
+    FOREIGN KEY (menu_id)
+        REFERENCES menu (menu_id)
+);
