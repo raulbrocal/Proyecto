2a7c165d5365e48cc73b7cea424173aa7f9e36d7
raulbrocal <raulbrocalberral16@gmail.com>
2023-04-13 18:30:03 +0200

    Actualización base de datos e insertar datos

--- sql/DDL_restaurant.sql
+++ sql/DDL_restaurant.sql
@@ -15,64 +15,72 @@ CREATE TABLE restaurant (
     closing_time TIME NOT NULL
 );
 
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    profile ENUM('CLIENT', 'EMPLOYEE') NOT NULL DEFAULT 'CLIENT'
+);
+
 CREATE TABLE reservation (
     reservation_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    user_id INT,
     phone VARCHAR(20) NOT NULL,
     email VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
     time TIME NOT NULL,
     number_of_people INT(11) UNSIGNED NOT NULL,
     indoor BOOLEAN NOT NULL DEFAULT TRUE,
-    comment VARCHAR(255)
-);
-
-CREATE TABLE user (
-    userId INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    profile ENUM('CLIENT', 'EMPLOYEE') NOT NULL DEFAULT 'CLIENT'
+    comment VARCHAR(255),
+    FOREIGN KEY (user_id)
+        REFERENCES user (user_id)
 );
 
 CREATE TABLE dinnerTable (
     number INT AUTO_INCREMENT PRIMARY KEY,
-    userId INT,
+    user_id INT,
     description VARCHAR(255),
     capacity INT NOT NULL,
-    FOREIGN KEY (userId)
-        REFERENCES user (userId)
+    FOREIGN KEY (user_id)
+        REFERENCES user (user_id)
 );
 
 CREATE TABLE menu (
     menu_id INT AUTO_INCREMENT PRIMARY KEY,
+    price DECIMAL(8 , 2 ) NOT NULL,
+    number_id INT NOT NULL,
+    drink_id INT,
+    dishes_id INT,
+    dessert_id INT,
+    FOREIGN KEY (number_id)
+        REFERENCES dinnerTable (number)
+);
+
+CREATE TABLE drink (
+    drink_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    description VARCHAR(255)
+    price DECIMAL(10 , 2 ) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    alcoholic BOOLEAN NOT NULL
 );
 
 CREATE TABLE dishes (
     dishes_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255),
-    price DECIMAL(10 , 2 ),
+    price DECIMAL(10 , 2 ) NOT NULL,
     type VARCHAR(255) NOT NULL,
     allergens VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE drink (
-    drink_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    price DECIMAL(10 , 2 ),
-    alcoholic BOOLEAN NOT NULL
 );
 
 CREATE TABLE dessert (
     dessert_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255),
-    price DECIMAL(10 , 2 ),
+    price DECIMAL(10 , 2 ) NOT NULL,
     allergens VARCHAR(50) NOT NULL
 );
 
