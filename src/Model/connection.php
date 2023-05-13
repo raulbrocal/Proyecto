3a063d0740c43cfad2a77e360607264c034a878a
raulbrocal <raulbrocalberral16@gmail.com>
2023-05-13 11:24:47 +0200

    Plantilla inicio terminada.

--- src/Model/connection.php
+++ src/Model/connection.php
@@ -1,12 +1,10 @@
 <?php
 try {
-    // Database connection data
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '1234');
     define('DB_NAME', 'restaurant');
 
-    // Connecting to the database
     $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
     return $conn;
--- src/View/index.php
+++ src/View/index.php
@@ -8,7 +8,7 @@
     <link rel="icon" href="../../img/logo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&family=Rubik+Mono+One&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -17,37 +17,38 @@
 </head>
 
 <body id="google_translate_element">
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-scroll fixed-top shadow-0 border-bottom border-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php"><img src="../../img/logo.png" alt="logo" style="max-height: 100%;"></a>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a href="menu.php"><button type="button" class="btn">Nuestra carta</button></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="registration.php"><button type="button" class="btn">Únete a
+                                    nosotros</button></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="reservation.php"><button type="button" class="btn">Reserva tu
+                                    mesa</button></a>
+                        </li>
+                        <li class="nav1-item">
+                            <a href="#"><button type="button" class="btn" id="translate_button">ES</button></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
     <section class="container-fluid title">
         <h1>Crew Bar</h1>
     </section>
 
     <div class="container-xxl">
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-scroll fixed-top shadow-0 border-bottom border-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="index.php"><img src="../../img/logo.png" alt="logo" style="max-height: 100%;"></a>
-                    <div class="collapse navbar-collapse">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item">
-                                <a href="menu.php"><button type="button" class="btn">Nuestra carta</button></a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="registration.php"><button type="button" class="btn">Únete a
-                                        nosotros</button></a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="reservation.php"><button type="button" class="btn">Reserva tu
-                                        mesa</button></a>
-                            </li>
-                            <li class="nav1-item">
-                                <a href="#"><button type="button" class="btn" id="translate_button">ES</button></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-
         <section class="banner">
             <div id="demo" class="carousel slide" data-bs-ride="carousel">
 
@@ -82,27 +83,23 @@
             </div>
         </section>
 
-        <section class="info">
-            <div class="container">
-                <div class="article">
-                    <img src="" alt="Chef">
-                    <h3 class="title">Our Chef</h3>
-                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.
-                        Aliquam erat volutpat.</p>
-                </div>
-                <div class="article">
-                    <img src="" alt="Interior">
-                    <h3 class="title">The Restaurant</h3>
-                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.
-                        Aliquam erat volutpat.</p>
-                </div>
-                <div class="article">
-                    <img src="" alt="Menu">
-                    <h3 class="title">Our Menu</h3>
-                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.
-                        Aliquam erat volutpat.</p>
-                </div>
+        <section class="about">
+            <div class="article">
+                <h3 class="title">Our Chef</h3>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.
+                    Aliquam erat volutpat.</p>
             </div>
+            <div class="article">
+                <h3 class="title">The Restaurant</h3>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.
+                    Aliquam erat volutpat.</p>
+            </div>
+            <div class="article">
+                <h3 class="title">Our Menu</h3>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.
+                    Aliquam erat volutpat.</p>
+            </div>
+
         </section>
 
         <footer>
